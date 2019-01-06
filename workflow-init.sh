1d9664c7b3ecc5da4b9bd557e5c495c8c765f80d
Dima Minka <cdk@cdk.co.il>
2019-01-06 12:08:47 +0200

    fix--workflow--cdk bedrock wp package changing

--- workflow-init.sh
+++ workflow-init.sh
@@ -43,7 +43,7 @@ elif [ "$conf_app_setup_workflow" == "cdk-comp/bedrock" ]; then
     rm -rf bedrock-master master.zip
     # Setup WordPress version from config
     if [ "$conf_app_setup_wordpress" != "*" ]; then
-        composer require roots/wordpress:$conf_app_setup_wordpress --update-no-dev
+        composer require johnpbloch/wordpress:$conf_app_setup_wordpress --update-no-dev
     else
         # Runing installation via composer
         composer install --no-dev
