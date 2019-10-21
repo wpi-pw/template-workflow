9bb9169ad9fa810b4cd48a8cf661cb1a6dd1d439
Dima Minka <bzx@mail.ru>
2019-10-21 13:30:43 +0300

    chore--workflow--don't run if exist web

--- wpi-workflow.sh
+++ wpi-workflow.sh
@@ -18,29 +18,9 @@ printf "${GRN}===================================${NC}\n"
 printf "${GRN}Running workflow $wpi_init_workflow${NC}\n"
 printf "${GRN}===================================${NC}\n"
 
-if [ "$wpi_init_workflow" == "bedrock" ]; then
+if [ "$wpi_init_workflow" == "bedrock" ] && ! [ -d ${PWD}/web ]; then
   # Clone the repo
   curl -LOks https://github.com/roots/bedrock/archive/master.zip
-  # Unzip the repo
-  unzip -q master.zip
-  # Prepare the project
-  mv bedrock-master/.env.example .
-  mv bedrock-master/config .
-  mv bedrock-master/web .
-  mv bedrock-master/composer.json .
-  mv bedrock-master/wp-cli.yml .
-  # Clean zip and cloned directory
-  rm -rf bedrock-master master.zip
-  # Setup WordPress version from config
-  if [ "$wpi_init_wordpress" != "*" ]; then
-      composer require roots/wordpress:$wpi_init_wordpress --update-no-dev --quiet
-  else
-      # Runing installation via composer
-      composer install --no-dev --quiet
-  fi
-elif [ "$wpi_init_workflow" == "cdk-comp/bedrock" ]; then
-  # Clone the repo
-  curl -LOks https://github.com/cdk-comp/bedrock/archive/master.zip
   # Unzip the repo
   unzip -q master.zip
   # Prepare the project
