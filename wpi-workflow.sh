2b111453d052b89af1d3c7411df616fda6b98323
DimaMinka <cdk@cdk.co.il>
2019-11-03 22:02:07 +0200

    chore--workflow--rebuild to yq done

--- wpi-workflow.sh
+++ wpi-workflow.sh
@@ -14,11 +14,11 @@ done
 # Get wpi-source for yml parsing, noroot, errors etc
 source <(curl -s https://raw.githubusercontent.com/wpi-pw/template-workflow/master/wpi-source.sh)
 
-printf "${GRN}===================================${NC}\n"
-printf "${GRN}Running workflow $wpi_init_workflow${NC}\n"
-printf "${GRN}===================================${NC}\n"
+printf "${GRN}========================================${NC}\n"
+printf "${GRN}Running workflow $(wpi_yq init.workflow)${NC}\n"
+printf "${GRN}========================================${NC}\n"
 
-if [ "$wpi_init_workflow" == "bedrock" ] && ! [ -d ${PWD}/web ]; then
+if [ "$(wpi_yq init.workflow)" == "bedrock" ] && ! [ -d ${PWD}/web ]; then
   # Clone the repo
   curl -LOks https://github.com/roots/bedrock/archive/master.zip
   # Unzip the repo
@@ -32,26 +32,28 @@ if [ "$wpi_init_workflow" == "bedrock" ] && ! [ -d ${PWD}/web ]; then
   # Clean zip and cloned directory
   rm -rf bedrock-master master.zip
   # Setup WordPress version from config
-  if [ "$wpi_init_wordpress" != "*" ]; then
-      composer require roots/wordpress:$wpi_init_wordpress --update-no-dev --quiet
+  if [ "$(wpi_yq init.wordpress)" != "null" ] && [ "$(wpi_yq init.wordpress)" != "*" ]; then
+      composer require roots/wordpress:$(wpi_yq init.wordpress) --update-no-dev --quiet
   else
       # Runing installation via composer
       composer install --no-dev --quiet
   fi
-elif [ "$wpi_init_workflow" == "wp-cli" ]; then
+elif [ "$(wpi_yq init.workflow)" == "wp-cli" ]; then
   # Get current env from arg if exist
-  wpi_db_name="wpi_env_$1_db_name"
-  wpi_db_user="wpi_env_$1_db_user"
-  wpi_db_pass="wpi_env_$1_db_pass"
-  wpi_db_prefix="wpi_env_$1_db_prefix"
+  wpi_db_name="env.$1.db_name"
+  wpi_db_user="env.$1.db_user"
+  wpi_db_pass="env.$1.db_pass"
+  wpi_db_prefix="env.$1.db_prefix"
+  wp_version=""
+
   # Setup WordPress version from config
-  if [ "$wpi_init_wordpress" != "*" ]; then
-      wp_version="--version=$wpi_init_wordpress"
+  if [ "$(wpi_yq init.wordpress)" != "*" ]; then
+      wp_version="--version=$(wpi_yq init.wordpress)"
   fi
   # Define WordPress web path
   echo "path: web" > wp-cli.yml
   # Download Wordpress
-  wp core download --path=web --quiet
+  wp core download --path=web $wp_version --force --quiet
   # Generate wp-config.php
-  wp core config --dbname="${!wpi_db_name}" --dbuser="${!wpi_db_user}" --dbpass="${!wpi_db_pass}" --dbprefix="${!wpi_db_prefix}" --dbhost=localhost --quiet
+  wp core config --dbname=$(wpi_yq $wpi_db_name) --dbuser=$(wpi_yq $wpi_db_user) --dbpass=$(wpi_yq $wpi_db_pass) --dbprefix=$(wpi_yq $wpi_db_prefix) --dbhost=localhost --quiet
 fi
